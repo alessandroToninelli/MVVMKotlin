c908c541b3878da7aa84f3eea669cb92a82c47ce
alessandro <alessandrotoninelli@gmail.com>
2018-09-16 16:32:34 +0200

    retry ciao ciao

--- app/src/main/java/it/toninelli/mvvmkotlin/ui/user/UserFragment.kt
+++ app/src/main/java/it/toninelli/mvvmkotlin/ui/user/UserFragment.kt
@@ -39,7 +39,7 @@ class UserFragment: Fragment(), Injectable{
 
         dataBinding.callback = object : RetryCallback {
             override fun retry() {
-                println("retry ciao")
+                println("retry ciao caio")
             }
         }
 
